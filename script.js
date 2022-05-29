d17e521ea3166f54ff5c4fae928bb955f11a9ee2
GadStam <gadstam71@gmail.com>
2022-05-29 17:30:19 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const nuevaTarea = event => {
+    event.preventDefault();
+    let value=document.getElementById("tareas").value
+    if(!value) return;
+    const task = document.createElement('div');
+    task.classList.add('task', 'roundBorder');
+    task.addEventListener('click', changeTaskState)
+    task.textContent = value;
+    tasksContainer.prepend(task);
+    event.target.reset();
+};
+
+const changeTaskState = event => {
+    event.target.classList.toggle('done');
+};
+
+
